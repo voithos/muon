11925dcf11b3cd0c7a8481011b272b7bce9cd8ed
Zaven Muradyan <zaven@voithos.io>
2021-01-03 19:17:15 -0800

    Add more sample scenes.

--- muon/types.h
+++ muon/types.h
@@ -8,6 +8,8 @@ namespace muon {
 // Common types and constants shared across muon.
 
 // TODO: Look into why this needs to be so large to avoid numerical issues.
+// TODO: Use a better way of managing intersection error (see
+// http://www.pbr-book.org/3ed-2018/Shapes/Managing_Rounding_Error.html).
 constexpr float kEpsilon = 0.0001;
 
 class Primitive;
