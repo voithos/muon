be8a10c2286ba0c69a53a0ab969bfe8ece8a2eb0
Zaven Muradyan <zaven@voithos.io>
2021-01-14 21:42:21 -0800

    Fix size type.

--- muon/film.cc
+++ muon/film.cc
@@ -7,8 +7,7 @@
 
 namespace muon {
 
-// TODO: These should be size_t.
-void Film::SetPixel(int x, int y, glm::vec3 color) {
+void Film::SetPixel(size_t x, size_t y, glm::vec3 color) {
   // Verify bounds of x and y.
   if (x < 0 || y < 0 || x >= width_ || y >= height_) {
     throw std::invalid_argument(
@@ -22,8 +21,8 @@ void Film::SetPixel(int x, int y, glm::vec3 color) {
 void Film::WriteOutput() {
   VLOG(1) << "Writing to output: " << output_file_;
 
-  for (int x = 0; x < width_; ++x) {
-    for (int y = 0; y < height_; ++y) {
+  for (size_t x = 0; x < width_; ++x) {
+    for (size_t y = 0; y < height_; ++y) {
       glm::vec3 value =
           accumulator_[x][y] / static_cast<float>(samples_per_pixel_);
       glm::vec3 color = glm::clamp(value, 0.0f, 1.0f);
--- muon/film.h
+++ muon/film.h
@@ -17,7 +17,8 @@ class Film {
  public:
   // Initializes a new Film with a given width and height and a path to an
   // output file, which should be a png.
-  Film(int width, int height, int samples_per_pixel, std::string output_file)
+  Film(size_t width, size_t height, size_t samples_per_pixel,
+       std::string output_file)
       : width_(width),
         height_(height),
         samples_per_pixel_(samples_per_pixel),
@@ -28,15 +29,15 @@ class Film {
   Film &operator=(Film &&other) = default;
 
   // Sets a specific pixel coordinate to a given color.
-  void SetPixel(int x, int y, glm::vec3 color);
+  void SetPixel(size_t x, size_t y, glm::vec3 color);
 
   // Writes the sampled output to disk.
   void WriteOutput();
 
  private:
-  int width_;
-  int height_;
-  int samples_per_pixel_;
+  size_t width_;
+  size_t height_;
+  size_t samples_per_pixel_;
   std::string output_file_;
   std::vector<std::vector<glm::vec3>> accumulator_;
   cimg_library::CImg<unsigned char> output_;
