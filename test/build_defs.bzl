e8c895885dee88ca00b7a33c3706d0fd21d447df
Zaven Muradyan <zaven@voithos.io>
2021-02-07 19:37:35 -0800

    Add support for nondeterministic diff tests.

--- test/build_defs.bzl
+++ test/build_defs.bzl
@@ -1,5 +1,15 @@
-def scene_diff_test(name, scene, golden, size="medium"):
+def scene_diff_test(name, scene, golden, truth=None, tolerance=None, size="medium"):
   """Creates a diff test for the given scene files."""
+  extra_args = []
+  extra_data = []
+  if truth != None:
+    # Nondeterministic test requested.
+    if tolerance == None:
+      fail("tolerance must be provided if a truth image is given")
+    extra_args.append("$(location {})".format(truth))
+    extra_args.append(tolerance)
+    extra_data.append(truth)
+
   native.sh_test(
       name = name,
       size = size,
@@ -7,12 +17,12 @@ def scene_diff_test(name, scene, golden, size="medium"):
       args = [
           "$(location {})".format(scene),
           "$(location {})".format(golden),
-      ],
+      ] + extra_args,
       data = [
           golden,
           scene,
           "//muon",
-      ],
+      ] + extra_data,
       deps = [
           "@bazel_tools//tools/bash/runfiles",
       ],
--- test/diff_test.sh
+++ test/diff_test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #
 # A modular test that renders a scene and compares it with a golden image.
-# Usage:
+# Since rendering is not always deterministic, this test allows specifying a
+# "ground truth" image in addition to the golden, which is then compared to
+# what the renderer generates by computing the normalized mean absolute error.
+#
+# Usage for determinstic tests:
 #   diff_test.sh <test_scene> <golden_image>
+#
+# Usage for nondeterministic tests:
+#   diff_test.sh <test_scene> <golden_image> <truth_image> <mae_tolerance>
 
 # --- begin runfiles.bash initialization v2 ---
 # Copy-pasted from the Bazel Bash runfiles library v2.
@@ -23,18 +30,25 @@ if ! hash compare &> /dev/null; then
   exit 1
 fi
 
-# Unpack arguments.
-SCENE="$1"
-GOLDEN="$2"
-
-if [[ "$SCENE" == "" ]]; then
-  echo "ERROR: Scene file not provided"
-  exit 1
-fi
-
 MUON="$(rlocation __main__/muon/muon || :)"
 if [[ ! -f $MUON ]]; then
   echo "ERROR: muon binary not found"
+  exit 1
+fi
+
+# Unpack arguments.
+SCENE="$1"
+GOLDEN="$2"
+TRUTH=""
+TOLERANCE=""
+
+if [[ $# -gt 2 ]]; then
+  TRUTH="$3"
+  TOLERANCE="$4"
+fi
+
+if [[ "$SCENE" == "" ]]; then
+  echo "ERROR: Scene file not provided"
   exit 1
 fi
 
@@ -52,9 +66,9 @@ fi
 
 # Renders an image via the given scene file and compares the output to the
 # golden.
-function test_diff {
+function test_diff_equality {
   OUTPUT_FILE="$TEST_UNDECLARED_OUTPUTS_DIR/output.png"
-  DIFF_FILE="$TEST_UNDECLARED_OUTPUTS_DIR/difference.png"
+  DIFF_FILE="$TEST_UNDECLARED_OUTPUTS_DIR/diff.png"
 
   # Render the image.
   $MUON --scene="$SCENE_FILE" --output="$OUTPUT_FILE"
@@ -63,10 +77,44 @@ function test_diff {
   abs_error=$(compare -metric AE "$OUTPUT_FILE" "$GOLDEN_IMAGE" "$DIFF_FILE" 2>&1 || :)
   echo "Absolute image error: $abs_error"
 
-  if (( abs_error > 0 )); then
+  if (( $(bc -l <<< "$abs_error > 0") )); then
     echo "ERROR: Image had error"
     exit 1
   fi
 }
 
-test_diff
+# Renders an image via the given scene file and compares the output's mean
+# absolute error to the ground truth file with that of the golden.
+function test_diff_mae {
+
+  OUTPUT_FILE="$TEST_UNDECLARED_OUTPUTS_DIR/output.png"
+  DIFF_FILE="$TEST_UNDECLARED_OUTPUTS_DIR/diff.png"
+  GOLDEN_DIFF_FILE="$TEST_UNDECLARED_OUTPUTS_DIR/golden_diff.png"
+
+  TRUTH_IMAGE="$(rlocation "__main__/$TRUTH" || :)"
+  if [[ ! -f $TRUTH_IMAGE ]]; then
+    echo "ERROR: Ground truth image __main__/$TRUTH not found"
+    exit 1
+  fi
+
+  # Render the image.
+  $MUON --scene="$SCENE_FILE" --output="$OUTPUT_FILE"
+
+  # Generate a diff image with the truth, and also generate a diff image with
+  # the golden.
+  mae_error=$(compare -metric MAE "$OUTPUT_FILE" "$TRUTH_IMAGE" "$DIFF_FILE" 2>&1 | cut -d "(" -f2 | cut -d ")" -f1 || :)
+  golden_mae_error=$(compare -metric MAE "$GOLDEN_IMAGE" "$TRUTH_IMAGE" "$GOLDEN_DIFF_FILE" 2>&1 | cut -d "(" -f2 | cut -d ")" -f1 || :)
+  echo "Normalized mean absolute error:            $mae_error"
+  echo "Normalized mean absolute error of golden:  $golden_mae_error"
+
+  if (( $(bc -l <<< "($golden_mae_error - $mae_error)^2 > $TOLERANCE^2") )); then
+    echo "ERROR: Image exceeded error tolerance"
+    exit 1
+  fi
+}
+
+if [[ "$TRUTH" != "" ]]; then
+  test_diff_mae
+else
+  test_diff_equality
+fi
